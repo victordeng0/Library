5383286c2856c3cf4eb99ed55f3354f010614f3f
victordeng0 <victordeng8@gmail.com>
2020-09-08 03:41:30 -0400

    created onclick event listener that pushes objects with user input into an array

---
+++ scripts/scripts.js
@@ -0,0 +1,37 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.sayName = function() {
+        console.log(title)
+    }
+}
+
+function addBookToLibrary() {
+    myLibrary.push(Book);
+}
+
+
+
+function readerPrompt() {
+    title = prompt('What is the name of your Book?');
+    author = prompt('Who is the author?');
+    pages = prompt('How many pages are in your book?');
+    read = prompt('Did you finish reading the book?');
+    
+}
+
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    readerPrompt()
+    const newBook = new Book(title, author, pages, read)
+    myLibrary.push(newBook);
+    console.log(myLibrary)
+    
+})
+
